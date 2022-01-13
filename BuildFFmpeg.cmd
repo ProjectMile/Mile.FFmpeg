9165d82288446eb8e64ba3b3144000cb4db5aac4
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-01-13 23:56:27 +0800

    Adjust enabled FFmpeg feature list.

--- BuildFFmpeg.cmd
+++ BuildFFmpeg.cmd
@@ -18,13 +18,15 @@ rem Bootstrap vcpkg
 rem call bootstrap-vcpkg.bat
 
 rem Set features list
-set FFmpegCoreFeatures=avcodec,avdevice,avfilter,avformat,avresample,swresample,swscale
-set FFmpegFeatures=%FFmpegCoreFeatures%,bzip2,fontconfig,freetype,iconv,lzma,mp3lame,openjpeg,opus,snappy,soxr,speex,theora,vorbis,webp,xml2,zlib,sdl2,modplug,opencl,openh264
+set FFmpegFeatures=avcodec,avdevice,avfilter,avformat,avresample,swresample,swscale
+set FFmpegFeatures=%FFmpegFeatures%,bzip2,iconv,lzma,opus,speex,vorbis,xml2,zlib
+set FFmpegFeatures=%FFmpegFeatures%,modplug,mp3lame,openh264,openjpeg,snappy,soxr,theora,webp
+rem ,fontconfig,freetype,sdl2,opencl
 rem NotAvailableInAllPlatform alsa,aom,ass,dav1d,fribidi,ilbc,nvcodec,opengl,ssh,tensorflow,tesseract,vpx
 
-rem FFmpegInteropX zlib,bzlib,lzma,libxml2,iconv,libdav1d,openssl 
-rem OBS Studio nvenc,amf,libx264,libopus,libvorbis,libvpx,libsrt 
-rem K-Lite w32threads,libdav1d,libspeex,libopencore-amrnb,libopencore-amrwb,avisynth,gnutls,gmp,libxml2,zlib
+rem FFmpegInteropX libdav1d,openssl 
+rem OBS Studio nvenc,amf,libx264,libvpx,libsrt 
+rem K-Lite w32threads,libdav1d,libopencore-amrnb,libopencore-amrwb,avisynth,gnutls,gmp
 
 rem Build FFmpeg via vcpkg
 vcpkg install ffmpeg[%FFmpegFeatures%]:x86-windows-static ffmpeg[%FFmpegFeatures%]:x64-windows-static ffmpeg[%FFmpegFeatures%]:arm-windows-static ffmpeg[%FFmpegFeatures%]:arm64-windows-static 

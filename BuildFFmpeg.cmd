ceb27eea23e8d335ef89266109e630b2987dcb37
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-01-14 22:43:00 +0800

    Integrate fontconfig and freetype support.

--- BuildFFmpeg.cmd
+++ BuildFFmpeg.cmd
@@ -18,10 +18,12 @@ rem Bootstrap vcpkg
 rem call bootstrap-vcpkg.bat
 
 rem Set features list
-set FFmpegFeatures=avcodec,avdevice,avfilter,avformat,avresample,swresample,swscale
-set FFmpegFeatures=%FFmpegFeatures%,bzip2,iconv,lzma,opus,speex,vorbis,xml2,zlib
-set FFmpegFeatures=%FFmpegFeatures%,modplug,mp3lame,openh264,openjpeg,snappy,soxr,theora,webp
-rem ,fontconfig,freetype,sdl2,opencl
+set FFmpegFeatures=avcodec,avdevice,avfilter,avformat,avresample,bzip2
+set FFmpegFeatures=%FFmpegFeatures%,fontconfig,freetype,iconv,lzma,modplug
+set FFmpegFeatures=%FFmpegFeatures%,mp3lame,openh264,openjpeg,snappy,soxr
+set FFmpegFeatures=%FFmpegFeatures%,speex,opus,swresample,swscale,theora
+set FFmpegFeatures=%FFmpegFeatures%,vorbis,webp,xml2,zlib
+rem ,sdl2,opencl
 rem NotAvailableInAllPlatform alsa,aom,ass,dav1d,fribidi,ilbc,nvcodec,opengl,ssh,tensorflow,tesseract,vpx
 
 rem FFmpegInteropX libdav1d,openssl 
@@ -29,6 +31,13 @@ rem OBS Studio nvenc,amf,libx264,libvpx,libsrt
 rem K-Lite w32threads,libdav1d,libopencore-amrnb,libopencore-amrwb,avisynth,gnutls,gmp
 
 rem Build FFmpeg via vcpkg
-vcpkg install ffmpeg[%FFmpegFeatures%]:x86-windows-static ffmpeg[%FFmpegFeatures%]:x64-windows-static ffmpeg[%FFmpegFeatures%]:arm-windows-static ffmpeg[%FFmpegFeatures%]:arm64-windows-static 
+vcpkg install ffmpeg[%FFmpegFeatures%]:x86-windows-static
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+vcpkg install ffmpeg[%FFmpegFeatures%]:x64-windows-static
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+vcpkg install ffmpeg[%FFmpegFeatures%]:arm-windows-static
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+vcpkg install ffmpeg[%FFmpegFeatures%]:arm64-windows-static 
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 
 @endlocal

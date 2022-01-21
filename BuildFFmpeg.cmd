4d3a4a9f3ea2224390576b58a1817e5e0c78093b
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-01-21 22:03:50 +0800

    Integrate dav1d, srt and vpx support.

--- BuildFFmpeg.cmd
+++ BuildFFmpeg.cmd
@@ -23,12 +23,12 @@ set FFmpegFeatures=%FFmpegFeatures%,fontconfig,freetype,iconv,lzma,modplug
 set FFmpegFeatures=%FFmpegFeatures%,mp3lame,openh264,openjpeg,snappy,soxr
 set FFmpegFeatures=%FFmpegFeatures%,speex,opus,swresample,swscale,theora
 set FFmpegFeatures=%FFmpegFeatures%,vorbis,webp,xml2,zlib
+set FFmpegFeatures=%FFmpegFeatures%,dav1d,srt,vpx
 rem ,sdl2,opencl
-rem NotAvailableInAllPlatform alsa,aom,ass,dav1d,fribidi,ilbc,nvcodec,opengl,ssh,tensorflow,tesseract,vpx
+rem NotAvailableInAllPlatform alsa,aom,ass,fribidi,ilbc,nvcodec,opengl,ssh,tensorflow,tesseract
 
-rem FFmpegInteropX libdav1d,openssl 
-rem OBS Studio nvenc,amf,libx264,libvpx,libsrt 
-rem K-Lite w32threads,libdav1d,libopencore-amrnb,libopencore-amrwb,avisynth,gnutls,gmp
+rem OBS Studio nvenc,amf,libx264 
+rem K-Lite w32threads,libopencore-amrnb,libopencore-amrwb,avisynth,gnutls,gmp
 
 rem Build FFmpeg via vcpkg
 vcpkg install ffmpeg[%FFmpegFeatures%]:x86-windows-static

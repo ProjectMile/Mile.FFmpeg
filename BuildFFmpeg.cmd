3459c485cc4e1f19be80fcab8288fa5b59463138
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-01-27 22:06:07 +0800

    Integrate ass and fribidi support.

--- BuildFFmpeg.cmd
+++ BuildFFmpeg.cmd
@@ -18,13 +18,13 @@ rem Bootstrap vcpkg
 call bootstrap-vcpkg.bat
 
 rem Set features list
-set FFmpegFeatures=aom,avcodec,avdevice,avfilter,avformat,bzip2,dav1d
-set FFmpegFeatures=%FFmpegFeatures%,fontconfig,freetype,iconv,ilbc,lzma,modplug
-set FFmpegFeatures=%FFmpegFeatures%,mp3lame,openh264,openjpeg,opus,snappy,soxr
-set FFmpegFeatures=%FFmpegFeatures%,speex,srt,swresample,swscale,theora,vorbis
-set FFmpegFeatures=%FFmpegFeatures%,vpx,webp,xml2,zlib
+set FFmpegFeatures=aom,ass,avcodec,avdevice,avfilter,avformat,bzip2,dav1d
+set FFmpegFeatures=%FFmpegFeatures%,fontconfig,freetype,fribidi,iconv,ilbc
+set FFmpegFeatures=%FFmpegFeatures%,lzma,modplug,mp3lame,openh264,openjpeg
+set FFmpegFeatures=%FFmpegFeatures%,opus,snappy,soxr,speex,srt,swresample
+set FFmpegFeatures=%FFmpegFeatures%,swscale,theora,vorbis,vpx,webp,xml2,zlib
 rem ,sdl2,opencl
-rem NotAvailableInAllPlatform alsa,ass,fribidi,nvcodec,opengl,ssh,tensorflow,tesseract
+rem NotAvailableInAllPlatform alsa,nvcodec,opengl,ssh,tensorflow,tesseract
 
 rem OBS Studio nvenc,amf,libx264 
 rem K-Lite w32threads,libopencore-amrnb,libopencore-amrwb,avisynth,gnutls,gmp

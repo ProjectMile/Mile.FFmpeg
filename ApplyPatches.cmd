02bc453d56efe611a16dc4869d4638527976419d
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-01-13 23:37:22 +0800

    Update patches.

--- ApplyPatches.cmd
+++ ApplyPatches.cmd
@@ -6,9 +6,9 @@ set PatchesFolder="%~dp0Patches"
 set VcpkgFolder="%~dp0Mile.FFmpeg.Vcpkg"
 
 pushd %VcpkgFolder%
-git am %PatchesFolder%\Only-build-release-versions-of-static-libraries.patch
+git am %PatchesFolder%\0001-Only-build-release-versions-of-static-libraries.patch
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
-git am %PatchesFolder%\Adjust-FFmpeg-port-dependencies-policy.patch
+git am %PatchesFolder%\0002-Adjust-FFmpeg-port-dependencies-policy.patch
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 popd
 

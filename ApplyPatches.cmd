026a972361b442837244cb3752a33c2a193f6007
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-01-14 00:51:37 +0800

    Make add-triplets-for-building-release-version-of-static-libraries-only.patch available.

--- ApplyPatches.cmd
+++ ApplyPatches.cmd
@@ -6,8 +6,8 @@ set PatchesFolder="%~dp0Patches"
 set VcpkgFolder="%~dp0Mile.FFmpeg.Vcpkg"
 
 pushd %VcpkgFolder%
-@REM git am %PatchesFolder%\0001-Only-build-release-versions-of-static-libraries.patch
-@REM if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+git am %PatchesFolder%\add-triplets-for-building-release-version-of-static-libraries-only.patch
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 git am %PatchesFolder%\adjust-ffmpeg-port-dependencies-policy.patch
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 popd

fe21dc4bfac130134e7a0b38a84d2192d50ad456
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-02-07 23:08:35 +0800

    Add GetRepositoryRoot method for Mile.FFmpeg.ModuleDefinitionGenerator.

--- Mile.FFmpeg.ModuleDefinitionGenerator/Program.cs
+++ Mile.FFmpeg.ModuleDefinitionGenerator/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace Mile.FFmpeg.ModuleDefinitionGenerator
+﻿using System.Diagnostics;
+
+namespace Mile.FFmpeg.ModuleDefinitionGenerator
 {
     internal class Program
     {
+        public static string GetRepositoryRoot()
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    FileName = "git.exe",
+                    Arguments = "rev-parse --show-toplevel"
+                }
+            };
+
+            if (process.Start())
+            {
+                process.WaitForExit();
+                if (process.ExitCode == 0)
+                {
+                    string? result = process.StandardOutput.ReadLine();
+                    if (result != null)
+                    {
+                        return Path.GetFullPath(result);
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+
         static void Main(string[] args)
         {
+            string Root = GetRepositoryRoot();
+
+            Console.WriteLine(Root);
+
             Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
         }
     }
 }

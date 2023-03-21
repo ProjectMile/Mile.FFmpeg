64d47184414c6ce3ab0117da4796c3e83f55ffd8
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-03-21 20:35:54 +0800

    Add non-NuGet version package build logic to BuildAllTargets.cmd.

--- BuildAllTargets.cmd
+++ BuildAllTargets.cmd
@@ -13,4 +13,19 @@ call "%~dp0Mile.Project.Windows\InitializeVisualStudioEnvironment.cmd"
 rem Build all targets
 MSBuild -binaryLogger:Output\BuildAllTargets.binlog -m BuildAllTargets.proj
 
+rem Build Symbol Package
+7z a -r Mile.FFmpeg.Symbols.zip .\Binaries\Release\*.pdb
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+
+rem Extract files from NuGet package to folder
+7z x Mile.FFmpeg.nupkg "-o.\Mile.FFmpeg"
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+
+rem Copy Documents
+rename .\Mile.FFmpeg\build\native\*.md *.txt
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+
+rem Build SDK Package
+7z a -r Mile.FFmpeg.zip .\Mile.FFmpeg\build\native\*.*
+
 @endlocal

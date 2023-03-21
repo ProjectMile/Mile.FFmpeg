7e3741a4ef33d2dd000f59cf52d96999ebe7d6ed
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-03-21 20:39:00 +0800

    Fix build issue in BuildAllTargets.cmd.

--- BuildAllTargets.cmd
+++ BuildAllTargets.cmd
@@ -14,18 +14,18 @@ rem Build all targets
 MSBuild -binaryLogger:Output\BuildAllTargets.binlog -m BuildAllTargets.proj
 
 rem Build Symbol Package
-7z a -r Mile.FFmpeg.Symbols.zip .\Binaries\Release\*.pdb
+7z a -r Output\Mile.FFmpeg.Symbols.zip .\Output\Binaries\Release\*.pdb
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 
 rem Extract files from NuGet package to folder
-7z x Mile.FFmpeg.nupkg "-o.\Mile.FFmpeg"
+7z x Output\Mile.FFmpeg.nupkg "-o.\Output\Mile.FFmpeg"
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 
 rem Copy Documents
-rename .\Mile.FFmpeg\build\native\*.md *.txt
+rename .\Output\Mile.FFmpeg\build\native\*.md *.txt
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 
 rem Build SDK Package
-7z a -r Mile.FFmpeg.zip .\Mile.FFmpeg\build\native\*.*
+7z a -r Output\Mile.FFmpeg.zip .\Output\Mile.FFmpeg\build\native\*.*
 
 @endlocal

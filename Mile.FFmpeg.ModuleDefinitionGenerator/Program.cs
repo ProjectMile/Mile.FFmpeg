74301fa743915571a76503566f0e05f9f7f926b9
MouriNaruto <Kenji.Mouri@outlook.com>
2025-04-03 00:22:59 +0800

    Simplify the Mile.FFmpeg.ModuleDefinitionGenerator implementation.

--- Mile.FFmpeg.ModuleDefinitionGenerator/Program.cs
+++ Mile.FFmpeg.ModuleDefinitionGenerator/Program.cs
@@ -1,125 +1,9 @@
 ﻿using Mile.Project.Helpers;
-using System.Text;
 
 namespace Mile.FFmpeg.ModuleDefinitionGenerator
 {
     internal class Program
     {
-        const int ImageArchiveStartSize = 8;
-        const string ImageArchiveStart = "!<arch>\n";
-        const string ImageArchiveEnd = "`\n";
-        const string ImageArchivePad = "\n";
-        const string ImageArchiveLinkerMember = "/               ";
-        const string ImageArchiveLongnamesMember = "//              ";
-        const string ImageArchiveHybridmapMember = "/<HYBRIDMAP>/   ";
-
-        const int ImageArchiveMemberHeaderNameSize = 16;
-        const int ImageArchiveMemberHeaderDateSize = 12;
-        const int ImageArchiveMemberHeaderUserIDSize = 6;
-        const int ImageArchiveMemberHeaderGroupIDSize = 6;
-        const int ImageArchiveMemberHeaderModeSize = 8;
-        const int ImageArchiveMemberHeaderSizeSize = 10;
-        const int ImageArchiveMemberHeaderEndHeaderSize = 2;
-
-        static void GetAllSymbolsFromStaticLibraryFile(
-            string FilePath,
-            string Filters,
-            ref List<string> Symbols)
-        {
-            string[] ConvertedFilters = Filters.Split(';');
-
-            byte[] Content = File.ReadAllBytes(FilePath);
-
-            int CurrentOffset = 0;
-
-            string CurrentImageArchiveStart = Encoding.ASCII.GetString(
-                Content,
-                CurrentOffset,
-                ImageArchiveStartSize);
-            if (CurrentImageArchiveStart != ImageArchiveStart)
-            {
-                return;
-            }
-
-            CurrentOffset += ImageArchiveStartSize;
-            string CurrentImageArchiveLinkerMember = Encoding.ASCII.GetString(
-                Content,
-                CurrentOffset,
-                ImageArchiveMemberHeaderNameSize);
-            if (CurrentImageArchiveLinkerMember != ImageArchiveLinkerMember)
-            {
-                return;
-            }
-
-            CurrentOffset += ImageArchiveMemberHeaderNameSize;
-            CurrentOffset += ImageArchiveMemberHeaderDateSize;
-            CurrentOffset += ImageArchiveMemberHeaderUserIDSize;
-            CurrentOffset += ImageArchiveMemberHeaderGroupIDSize;
-            CurrentOffset += ImageArchiveMemberHeaderModeSize;
-            CurrentOffset += ImageArchiveMemberHeaderSizeSize;
-            string CurrentImageArchiveMemberHeaderEndHeader =
-                Encoding.ASCII.GetString(
-                    Content,
-                    CurrentOffset,
-                    ImageArchiveMemberHeaderEndHeaderSize);
-            if (CurrentImageArchiveMemberHeaderEndHeader != ImageArchiveEnd)
-            {
-                return;
-            }
-
-            CurrentOffset += ImageArchiveMemberHeaderEndHeaderSize;
-            int SymbolsCount = 0;
-            {
-                byte[] RawBytes = new Span<byte>(
-                    Content,
-                    CurrentOffset,
-                    4).ToArray();
-                if (BitConverter.IsLittleEndian)
-                {
-                    Array.Reverse(RawBytes);
-                }
-
-                SymbolsCount = BitConverter.ToInt32(
-                    RawBytes);
-            }
-            if (SymbolsCount == 0)
-            {
-                return;
-            }
-
-            CurrentOffset += sizeof(uint);
-            CurrentOffset += sizeof(uint) * SymbolsCount;
-            {
-                string[] RawStrings = Encoding.ASCII.GetString(
-                    Content,
-                    CurrentOffset,
-                    Content.Length - CurrentOffset).Split('\0');
-
-                for (int i = 0; i < SymbolsCount; ++i)
-                {
-                    bool Excluded = true;
-                    foreach (string Filter in ConvertedFilters)
-                    {
-                        if (RawStrings[i].StartsWith(Filter) ||
-                            RawStrings[i].StartsWith("_" + Filter))
-                        {
-                            Excluded = false;
-                            break;
-                        }
-                    }
-                    if (Excluded)
-                    {
-                        continue;
-                    }
-
-                    Symbols.Add(
-                        RawStrings[i].StartsWith("_")
-                        ? RawStrings[i].Substring(1)
-                        : RawStrings[i]);
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
             string ProjectRootPath = GitRepository.GetRootPath();
@@ -171,10 +55,32 @@ namespace Mile.FFmpeg.ModuleDefinitionGenerator
 
                 foreach (KeyValuePair<string, string> File in Files)
                 {
-                    GetAllSymbolsFromStaticLibraryFile(
-                        File.Key,
-                        File.Value,
-                        ref Symbols);
+                    string[] ConvertedFilters = File.Value.Split(';');
+                    ImageArchive.Archive Archive = ImageArchive.Parse(File.Key);
+                    if (Archive.Symbols != null)
+                    {
+                        SortedSet<string> OriginalSymbols =
+                            ImageArchive.ListSymbols(
+                                Archive.Symbols,
+                                File.Key.Contains("x86"));
+                        foreach (string Symbol in OriginalSymbols)
+                        {
+                            bool Excluded = true;
+                            foreach (string Filter in ConvertedFilters)
+                            {
+                                if (Symbol.StartsWith(Filter))
+                                {
+                                    Excluded = false;
+                                    break;
+                                }
+                            }
+                            if (Excluded)
+                            {
+                                continue;
+                            }
+                            Symbols.Add(Symbol);
+                        }
+                    }
                 }
 
                 Console.WriteLine(Symbols.Count);

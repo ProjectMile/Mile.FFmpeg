49576018f872bfbfd84f8e4f4c5425043c753828
MouriNaruto <Kenji.Mouri@outlook.com>
2025-04-02 23:45:06 +0800

    Simplify the BuildFFmpeg.cmd.

--- BuildFFmpeg.cmd
+++ BuildFFmpeg.cmd
@@ -17,26 +17,14 @@ cd "%~dp0Mile.FFmpeg.Vcpkg"
 rem Bootstrap vcpkg
 call bootstrap-vcpkg.bat
 
-rem Set features list
-set FFmpegFeatures=aom,ass,avcodec,avdevice,avfilter,avformat,bzip2,dav1d
-set FFmpegFeatures=%FFmpegFeatures%,fontconfig,freetype,fribidi,iconv,ilbc
-set FFmpegFeatures=%FFmpegFeatures%,lzma,modplug,mp3lame,openh264,openjpeg
-set FFmpegFeatures=%FFmpegFeatures%,opus,snappy,soxr,speex,srt,swresample
-set FFmpegFeatures=%FFmpegFeatures%,swscale,theora,vorbis,vpx,webp,xml2,zlib
-rem ,sdl2,opencl
-rem NotAvailableInAllPlatform alsa,nvcodec,opengl,ssh,tensorflow,tesseract
-
-rem OBS Studio nvenc,amf,libx264 
-rem K-Lite w32threads,libopencore-amrnb,libopencore-amrwb,avisynth,gnutls,gmp
-
 set VcpkgOptions= --overlay-triplets=..\Mile.FFmpeg.Vcpkg.ExtraTriplets
 
 rem Build FFmpeg via vcpkg
-vcpkg install %VcpkgOptions% ffmpeg[%FFmpegFeatures%]:x86-windows-static-release
+vcpkg install %VcpkgOptions% ffmpeg[all]:x86-windows-static-release
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
-vcpkg install %VcpkgOptions% ffmpeg[%FFmpegFeatures%]:x64-windows-static-release
+vcpkg install %VcpkgOptions% ffmpeg[all]:x64-windows-static-release
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
-vcpkg install %VcpkgOptions% ffmpeg[%FFmpegFeatures%]:arm64-windows-static-release
+vcpkg install %VcpkgOptions% ffmpeg[all]:arm64-windows-static-release
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 
 @endlocal
